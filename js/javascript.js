6acac977f60648f3ee0fdf6863475833ab671eb4
Wallace <wallace.silva35@fatec.sp.gov.br>
2023-08-27 22:04:48 -0300

    closes #4 - Modal

--- js/javascript.js
+++ js/javascript.js
@@ -6,32 +6,64 @@ var prods = [
     { id: 5, name: "Salada", price: 8.0 },
     { id: 6, name: "Torresmo", price: 12.0 },
 ];
-
-function calc(){
+function calc() {
     var quantities = document.getElementsByName("quantity");
-    var output     = document.getElementById("output");
-    var total      = 0;
-    var nome = document.getElementById("nome");
+    var output = document.getElementById("output");
+    var total = 0;
+    var nome = document.getElementById("nome").value;
     output.innerHTML = "";
-   
+    var modalContent = "";
     var formatter = new Intl.NumberFormat('pt-BR', {
         style: 'currency',
         currency: 'BRL',
     });
-    output.innerHTML += `<div class="title_name">Caro&nbsp;<div class="negrito">${nome.value}</div><br><br><br>`;
 
 
-output.innerHTML+=`Seguem os dados do seu pedido. </br></br>`;
+ 
 
-output.innerHTML+=`O seu pedido é: </br></br>`;
+     modalContent = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Java</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <h3>Caro: ${nome}</h3>
+                    <h5>Seguem os dados do seu pedido:</h5>`;
+
     for (var input of quantities) {
         var id = input.id;
-if(input.value>0){
-        output.innerHTML += `Prato: ${prods[id-1].name}  - Preço Unitário: ${formatter.format(prods[id-1].price)} - Quantidade: ${input.value} - Total ${formatter.format(prods[id-1].price * parseFloat(input.value))}. </br>`;
-        total            += prods[id-1].price * parseFloat(input.value);
+        if (input.value > 0) {
+            modalContent += `Prato: ${prods[id - 1].name} - Preço Unitário: ${formatter.format(prods[id - 1].price)} - Quantidade: ${input.value} - Total ${formatter.format(prods[id - 1].price * parseFloat(input.value))}. <br>`;
+            total += prods[id - 1].price * parseFloat(input.value);
+        }
     }
-    }
-    output.innerHTML += `</br></br><h2>Preço final: ${formatter.format(total)}</h2><br><br>`;
 
+    if(total>0){
+    modalContent += `<br><br><h2>Preço final: ${formatter.format(total)}</h2><br><br>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>`;
+    }else{
+        modalContent +=  `<br><br><h2>Erro: Você não selecionou nenhum prato</h2><br><br>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+    </div>
+</div>
+</div>`;
+
+    }
+    output.innerHTML = modalContent;
+
+    // Recreate the modal
+    $('#exampleModal').modal('show');
+    output.innerHTML = "";
+    modalContent = "";
 }
-
